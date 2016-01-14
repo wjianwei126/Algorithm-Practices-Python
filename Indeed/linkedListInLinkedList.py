02f09c3eed08923262ae0075babd561d05cfc62a
Zhuo li <lizhuogo@gmail.com>
2016-01-14 16:57:09 -0500

    new problem

--- Indeed/linkedListInLinkedList.py
+++ Indeed/linkedListInLinkedList.py
@@ -1,11 +1,38 @@
 class Node:
-    def __init__(self, val):
-        self.val = val
+    def __init__(self, size):
+        self.val = []
+        self.maxSize = size
         self.next = None
 
-class SuperNode:
-    def __init__(self, ma):
-        self.head = None
-        self.maxSize = 10
-        self.size = 0
-        self.next = None
+# class SuperNode:
+#     def __init__(self, ma):
+#         self.head = None
+#         self.maxSize = 10
+#         self.size = 0
+#         self.next = None
+
+class LinkedListOfArrays:
+    def __init__(self, head):
+        self.head = head
+
+    def insert(self, value, index):
+        fkhead = Node(0)
+        fkhead.next = self.head
+        node = fkhead
+        count = 0
+        while node.next and count < index:
+            node = node.next
+            count += 1
+
+        if count < index:
+            node.next = Node(10)
+            node.next.val.append(value)
+            return
+
+        if len(node.val) < node.maxSize:
+            node.val.append(value)
+        else:
+            temp = node.next
+            node.next = Node(10)
+            node.next.val.append(value)
+            node.next.next = temp
