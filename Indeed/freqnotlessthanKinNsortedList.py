9da5a167066e1dd4931f6f58991ce00a73cfaf01
Zhuo li <lizhuogo@gmail.com>
2016-01-14 20:49:24 -0600

    modified

--- Indeed/freqnotlessthanKinNsortedList.py
+++ Indeed/freqnotlessthanKinNsortedList.py
@@ -1,12 +1,26 @@
 import heapq
-def findFreqGreaterThanK(lists, k):
-    if not lists: return []
+class Stream:
+    def __init__(self, inputList):
+        self.list = inputList
+        self.index = 0
+
+    def hasNext(self):
+        return self.index < len(self.list)
+
+    def next(self):
+        if not self.hasNext():
+            raise IndexError('No more value!')
+        self.index += 1
+        return self.list[self.index-1]
+
+def findFreqGreaterThanK(streams, k):
+    if not streams: return []
     res = []
     heap = []
-    for i in range(len(lists)):
-        if len(lists[i]) == 0:
+    for i in range(len(streams)):
+        if not streams[i].hasNext():
             continue
-        heapq.heappush(heap, (lists[i][0], i))
+        heapq.heappush(heap, (streams[i].next(), i))
 
     while len(heap) >= k:
         curVal = None
@@ -18,18 +32,24 @@ def findFreqGreaterThanK(lists, k):
                 curVal = val
             if curVal == val:
                 count += 1
-                while lists[index] and lists[index][0] == curVal:
-                    lists[index].pop(0)
-                if lists[index]:
-                    heapq.heappush(heap, (lists[index][0], index))
+                if streams[index].hasNext():
+                    nextValue = streams[index].next()
+                    while streams[index].hasNext() and nextValue == curVal:
+                        nextValue = streams[index].next()
+                    if nextValue != curVal:
+                        heapq.heappush(heap, (nextValue, index))
             else:
-                heapq.heappush(heap, (lists[index][0], index))
+                heapq.heappush(heap, (val, index))
                 break
-        # print 'end round: ', curVal, heap, count
+
         if count >= k:
             res.append(curVal)
 
     return res
 
-lists = [[1,2,3,4,5,6,7,8,9,10], [1], [1,2,4,5,7,8,10], [2,4], [1,3,4,6,7,9,10]]
-print findFreqGreaterThanK(lists, 4)
+s1 = Stream([1,2,3,4,5,6,7,8,9,10])
+s2 = Stream([1])
+s3 = Stream([1,2,4,5,7,8,10])
+s4 = Stream([2,4])
+s5 = Stream([1,3,4,6,7,9,10])
+print findFreqGreaterThanK([s1,s2,s3,s4,s5], 4)
